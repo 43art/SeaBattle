984ea8eef52d0955617337b47bd784ce3ad69596
GeorgyHim <georgyhim1997@yandex.ru>
2021-04-01 14:41:23 +0300

    Add threads and class for every player

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,52 +5,11 @@ using System.Text;
 using System.Threading;
 
 namespace Server {
-    class ClientConnection {
-        TcpClient client;
-
-        public ClientConnection(TcpClient nclient) {
-            this.client = nclient;
-        }
-
-        private void processRequest(string message, NetworkStream stream) {
-            // TODO:
-            byte[] data = Encoding.Unicode.GetBytes("SERVER ACCEPTED: " + message);
-            stream.Write(data, 0, data.Length); ;
-        }
-
-        public void process() {
-            NetworkStream stream = null;
-            try {
-                stream = client.GetStream();
-                byte[] data = new byte[1024];
-                while (true) {
-                    StringBuilder sb = new StringBuilder();
-                    int bytes = 0;
-                    do {
-                        bytes = stream.Read(data, 0, data.Length);
-                        sb.Append(Encoding.UTF8.GetString(data, 0, bytes));
-                    }
-                    while (stream.DataAvailable);
-
-                    string message = sb.ToString();
-                    Console.WriteLine(message);
-                    processRequest(message, stream);
-                }
-            }
-            catch (Exception e) {
-                Console.WriteLine(e.Message);
-            }
-            finally {
-                if (stream != null)
-                    stream.Close();
-                if (client != null)
-                    client.Close();
-            }
-        }
-    }
 
     class Program {
         const int port = 6969; // порт для прослушивания подключений
+        static ClientConnection client1;
+        static ClientConnection client2;
 
         static void Main(string[] args) {
             TcpListener listener = null;
@@ -62,11 +21,21 @@ namespace Server {
 
                 while (true) {
                     TcpClient client = listener.AcceptTcpClient();
-                    ClientConnection cc = new ClientConnection(client);
-
+                    if (client1 != null && client2 != null)
+                        break;
                     Console.WriteLine("Client connected!");
-                    Thread clientThread = new Thread(new ThreadStart(cc.process));
-                    clientThread.Start();
+
+                    if (client1 == null) {
+                        client1 = new ClientConnection(client, 1);
+                        Thread clientThread = new Thread(new ThreadStart(client1.process));
+                        clientThread.Start();
+                    }
+                    else {
+                        client2 = new ClientConnection(client, 2, client1);
+                        client1.otherPlayer = client2;
+                        Thread clientThread = new Thread(new ThreadStart(client2.process));
+                        clientThread.Start();
+                    }
                 }
             }
             catch (Exception e) {
